a35fb7b914e1fd74ffa0129b496fef55a5ce6686
SanudaKJ <sanudajayasinghe18@gmail.com>
2026-02-17 20:17:45 +0530

    refactor: simplify serverless handler export in index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,10 @@
-// api/index.ts
 import serverless from "serverless-http";
 import app from "../src/app";
 import { connectDB } from "../src/db";
 
-const serverlessHandler = serverless(app);
+const handler = serverless(app);
 
-export const handler = async (req: any, res: any) => {
+export default async function (req: any, res: any) {
   await connectDB();
-  return serverlessHandler(req, res);
-};
+  return handler(req, res);
+}
